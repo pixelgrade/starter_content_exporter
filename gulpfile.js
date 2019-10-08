690346a549405572b919dd587036b4134def00f4
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-10-08 17:00:09 +0300

    Updated to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,10 @@
 var plugin = 'starter_content_exporter',
-	source_SCSS = { admin: './scss/**/*.scss', public: './scss/**/*.scss'},
-	dest_CSS = { admin:'./css/', public: './css/'},
 
 	gulp 		= require('gulp'),
-	exec 		= require('gulp-exec'),
-	concat 		= require('gulp-concat'),
-	notify 		= require('gulp-notify'),
-	chmod 		= require('gulp-chmod'),
+	plugins 	= require('gulp-load-plugins' )(),
 	fs          = require('fs'),
-	del         = require('del'),
-	rsync 		= require('gulp-rsync'),
-	replace 	= require('gulp-replace'),
-	rename 		= require('gulp-rename');
+	cp = require('child_process'),
+	del         = require('del');
 
 require('es6-promise').polyfill();
 
@@ -21,17 +14,33 @@ var options = {
 };
 
 /**
- * Copy theme folder outside in a build folder, recreate styles before that
+ * Copy plugin folder outside in a build folder, recreate styles before that
  */
 gulp.task( 'copy-folder', function() {
-	return gulp.src( './' )
-		.pipe( exec( 'rm -Rf ./../build; mkdir -p ./../build/starter_content_exporter; cp -Rf ./* ./../build/starter_content_exporter/' ) );
+	var dir = process.cwd();
+	return gulp.src( './*' )
+		.pipe( plugins.exec( 'rm -Rf ./../build; mkdir -p ./../build/' + plugin + ';', {
+			silent: true,
+			continueOnError: true // default: false
+		} ) )
+		.pipe(plugins.rsync({
+			root: dir,
+			destination: '../build/' + plugin + '/',
+			// archive: true,
+			progress: false,
+			silent: false,
+			compress: false,
+			recursive: true,
+			emptyDirectories: true,
+			clean: true,
+			exclude: ['node_modules']
+		}));
 } );
 
 /**
  * Clean the folder of unneeded files and folders
  */
-gulp.task( 'build', ['copy-folder'], function() {
+gulp.task( 'remove-files', function() {
 
 	// files that should not be present in build zip
 	var files_to_remove = [
@@ -80,19 +89,47 @@ gulp.task( 'build', ['copy-folder'], function() {
 		'socket/gulpfile.js',
 		'socket/package.json',
 		'socket/semantic.json',
+		'README.md',
 	];
 
 	files_to_remove.forEach( function( e, k ) {
-		files_to_remove[k] = '../build/starter_content_exporter/' + e;
+		files_to_remove[k] = '../build/' + plugin + '/' + e;
 	} );
 
-	del.sync( files_to_remove, { force: true } );
+	return del(files_to_remove, {force: true});
 } );
+
+function maybeFixBuildDirPermissions(done) {
+
+	cp.execSync('find ./../build -type d -exec chmod 755 {} \\;');
+
+	return done();
+}
+maybeFixBuildDirPermissions.description = 'Make sure that all directories in the build directory have 755 permissions.';
+gulp.task( 'fix-build-dir-permissions', maybeFixBuildDirPermissions );
+
+function maybeFixBuildFilePermissions(done) {
+
+	cp.execSync('find ./../build -type f -exec chmod 644 {} \\;');
+
+	return done();
+}
+maybeFixBuildFilePermissions.description = 'Make sure that all files in the build directory have 644 permissions.';
+gulp.task( 'fix-build-file-permissions', maybeFixBuildFilePermissions );
+
+function maybeFixIncorrectLineEndings(done) {
+
+	cp.execSync('find ./../build -type f -print0 | xargs -0 -n 1 -P 4 dos2unix');
+
+	return done();
+}
+maybeFixIncorrectLineEndings.description = 'Make sure that all line endings in the files in the build directory are UNIX line endings.';
+gulp.task( 'fix-line-endings', maybeFixIncorrectLineEndings );
 
 /**
  * Create a zip archive out of the cleaned folder and delete the folder
  */
-gulp.task( 'zip', ['build'], function() {
+gulp.task( 'make-zip', function() {
 	var versionString = '';
 	// get plugin version from the main plugin file
 	var contents = fs.readFileSync("./" + plugin + ".php", "utf8");
@@ -115,6 +152,18 @@ gulp.task( 'zip', ['build'], function() {
 	versionString = '-' + versionString.replace(/\./g, '-');
 
 	return gulp.src('./')
-		.pipe(exec('cd ./../; rm -rf ' + plugin[0].toUpperCase() + plugin.slice(1) + '*.zip; cd ./build/; zip -r -X ./../' + plugin[0].toUpperCase() + plugin.slice(1) + versionString + '.zip ./; cd ./../; rm -rf build'));
+		.pipe(plugins.exec('cd ./../; rm -rf ' + plugin[0].toUpperCase() + plugin.slice(1) + '*.zip; cd ./build/; zip -r -X ./../' + plugin[0].toUpperCase() + plugin.slice(1) + versionString + '.zip ./; cd ./../; rm -rf build'));
 
 } );
+
+function buildSequence(cb) {
+	return gulp.series( 'copy-folder', 'remove-files', 'fix-build-dir-permissions', 'fix-build-file-permissions', 'fix-line-endings' )(cb);
+}
+buildSequence.description = 'Sets up the build folder';
+gulp.task( 'build', buildSequence );
+
+function zipSequence(cb) {
+	return gulp.series( 'build', 'make-zip' )(cb);
+}
+zipSequence.description = 'Creates the zip file';
+gulp.task( 'zip', zipSequence  );
