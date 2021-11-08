78152a55733e87bf067d5999cdfaa14c734bd168
Vlad Olaru <vlad@pixelgrade.com>
2021-11-08 11:39:12 +0200

    Version bump to 1.2.2

--- starter_content_exporter.php
+++ starter_content_exporter.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Starter Content Exporter
  * Plugin URI:        https://pixelgrade.com/
  * Description:       A plugin which exposes exportable data through the REST API.
- * Version:           1.2.1
+ * Version:           1.2.2
  * Author:            Pixelgrade, Vlad Olaru
  * Author URI:        https://pixelgrade.com/
  * License:           GPL-2.0+
